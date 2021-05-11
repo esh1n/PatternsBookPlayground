71ab3a4881ad08b3ea2568c52049239a5efc6ef3
esh1n <eshinsergey@gmail.com>
2021-05-11 15:14:11 +0300

    fixed strategy patter usage

--- src/main/kotlin/StrategyPatternExample.kt
+++ src/main/kotlin/StrategyPatternExample.kt
@@ -1,103 +1,91 @@
 object StrategyPatternExample {
     @JvmStatic
     fun main(args: Array<String>) {
-        System.out.println("Strategy Pattern Example with Ducks")
-        val ducksInSwimmingPool = arrayOf(MallardDuck(),ToyDuck())
+        println("Strategy Pattern Example with Ducks")
+        val ducksInSwimmingPool = arrayOf(MallardDuck(), ToyDuck())
         ducksInSwimmingPool.forEach { duck ->
-            duck.display()
-            duck.performFly()
-            duck.performQuack()
+            duck.run {
+                display()
+                performFly()
+                performQuack()
+            }
         }
-        val toyDuck = ToyDuck()
-        toyDuck.setFlyBehavior(FlyRocketPowered())
-        toyDuck.display()
-        toyDuck.performFly()
-        toyDuck.swim()
+        ToyDuck().run {
+            flyBehaviour = FlyRocketPowered()
+            display()
+            performFly()
+            swim()
+        }
+
     }
 
-    class MallardDuck():Duck(){
+    class MallardDuck : Duck(flyBehaviour = FlyWithWings(), quackBehaviour = Squeak()) {
 
-        init {
-            flyBehaviour = FlyWithWings()
-            quackBehaviour = Squeak()
-        }
         override fun display() {
-            System.out.println("I am real mallard duck")
+            println("I am real mallard duck")
         }
 
     }
 
 
-    class ToyDuck():Duck(){
+    class ToyDuck : Duck(flyBehaviour = NoFly(),quackBehaviour = MuteQuack()) {
 
-        init {
-            flyBehaviour = NoFly()
-            quackBehaviour = MuteQuack()
-        }
         override fun display() {
-            System.out.println("I am toy duck")
+            println("I am toy duck")
         }
-
-
     }
 
-    abstract class Duck {
-        protected lateinit var flyBehaviour:FlyBehavior
-        protected lateinit var quackBehaviour:QuackBehavior
+    abstract class Duck(var flyBehaviour: FlyBehavior, var quackBehaviour: QuackBehavior) {
         abstract fun display()
-        fun swim(){
-            System.out.println("All ducks float, even decoys!")
+
+        fun swim() {
+            println("All ducks float, even decoys!")
         }
-        fun performFly(){
+
+        fun performFly() {
             flyBehaviour.fly()
         }
-        fun performQuack(){
+
+        fun performQuack() {
             quackBehaviour.quack()
         }
-
-        fun setFlyBehavior(flyBehavior: FlyBehavior){
-            this.flyBehaviour = flyBehaviour
-        }
-        fun setQuackBehavior(){
-            this.quackBehaviour = quackBehaviour
-        }
     }
 
-    class Quack:QuackBehavior{
+    class Quack : QuackBehavior {
         override fun quack() {
-            System.out.println("quack")
+            println("quack")
         }
 
     }
 
-    class MuteQuack:QuackBehavior{
+    class MuteQuack : QuackBehavior {
         override fun quack() {
-            System.out.println("--Silence--")
+            println("--Silence--")
         }
 
     }
 
-    class Squeak:QuackBehavior{
+    class Squeak : QuackBehavior {
         override fun quack() {
-            System.out.println("--squeak--")
+            println("--squeak--")
         }
     }
 
-    class FlyWithWings:FlyBehavior{
+    class FlyWithWings : FlyBehavior {
         override fun fly() {
-            System.out.println("--fly with wings--")
+            println("--fly with wings--")
         }
     }
 
-    class FlyRocketPowered:FlyBehavior{
+    class FlyRocketPowered : FlyBehavior {
         override fun fly() {
-            System.out.println("--Red Rocket!!!--")
+            println("--Red Rocket!!!--")
         }
     }
 
-    class NoFly:FlyBehavior{
+    class NoFly : FlyBehavior {
         override fun fly() {
-            System.out.println("--no fly--")
+            println("--no fly--")
         }
     }
 
